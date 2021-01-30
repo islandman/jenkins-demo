102bf1cec1f90a4652d4484b732a085bd93306fa
islandman <caljames1952@git.com>
2021-01-30 18:53:21 -0500

    added pipeline 2

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,3 +1,21 @@
+pipelineJob('pipelineJob1') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob1.groovy'))
+            sandbox()
+        }
+    }
+}
+
+pipelineJob('pipelineJob2') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob2.groovy'))
+            sandbox()
+        }
+    }
+}
+
 pipelineJob('pipelineJob') {
     definition {
         cps {
@@ -7,11 +25,3 @@ pipelineJob('pipelineJob') {
     }
 }
 
-pipelineJob('pipelineJob1') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob1.groovy'))
-            sandbox()
-        }
-    }
-}
